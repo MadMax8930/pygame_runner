4c4825700b74b8b9f2c89f48a62c8a481f243c7f
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-05-17 10:40:06 +0200

    enemy spawn logic

--- main.py
+++ main.py
@@ -2,6 +2,28 @@ import pygame
 from sys import exit
 from random import randint
 
+def obstacle_movement(obs_list):
+   if obs_list:
+      for obs_rect in obs_list:
+         obs_rect.x -= 5
+         
+         if obs_rect.bottom == 300:
+            screen.blit(snail_surface, obs_rect) 
+         else:
+            screen.blit(fly_surface, obs_rect)   
+         
+      # only copy to the list items that are on the screen
+      obs_list = [obs for obs in obs_list if obs.x > -100]  
+      return obs_list
+   else: return []
+
+def display_score():
+   current_time = (pygame.time.get_ticks() - start_time) // 1000
+   text_surface = my_font.render(f'Score: {current_time}', False, (64, 64, 64))
+   text_rectangle = text_surface.get_rect(center= (400, 50))
+   screen.blit(text_surface, text_rectangle)
+   return current_time
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner Pygame')
@@ -11,13 +33,11 @@ game_active = False
 start_time = 0
 score = 0
 
-# Game screen
+# Game screen & Obstacles
 sky_surface = pygame.image.load('graphics/sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-
-# Obstacles
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_rectangle = snail_surface.get_rect(bottomright= (600, 300))
+fly_surface = pygame.image.load('graphics/fly/fly1.png').convert_alpha()
 obstacle_list = []
 
 # Player
@@ -25,7 +45,7 @@ player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_
 player_rectangle = player_surface.get_rect(midbottom= (80, 300))
 player_gravity = 0
 
-# Intro screen
+# Intro & Game over screen
 player_stand = pygame.image.load('graphics/player/player_stand.png').convert_alpha()
 player_stand = pygame.transform.rotozoom(player_stand, 0, 2) # overwritten
 player_stand_rectangle = player_stand.get_rect(center= (400, 200))
@@ -36,22 +56,7 @@ game_info_rectangle = game_info.get_rect(center= (400, 330))
 
 # Timer
 obstacle_timer = pygame.USEREVENT + 1 # avoid conflicts
-pygame.time.set_timer(obstacle_timer, 900)
-
-def obstacle_movement(obs_list):
-   if obs_list:
-      for obs_rect in obs_list:
-         obs_rect.x -= 5
-         screen.blit(snail_surface, obs_rect)     
-      return obs_list
-   else: return []
-
-def display_score():
-   current_time = (pygame.time.get_ticks() - start_time) // 1000
-   text_surface = my_font.render(f'Score: {current_time}', False, (64, 64, 64))
-   text_rectangle = text_surface.get_rect(center= (400, 50))
-   screen.blit(text_surface, text_rectangle)
-   return current_time
+pygame.time.set_timer(obstacle_timer, 1500)
 
 while True:
    for event in pygame.event.get():
@@ -67,12 +72,14 @@ while True:
             if event.key == pygame.K_SPACE and player_rectangle.bottom >= 300:
                   player_gravity = -20      
          if event.type == obstacle_timer: # spawn obstacles
-            obstacle_list.append(snail_surface.get_rect(bottomright= (randint(900, 1100), 300)))
-            
+            if randint(0, 2 ): # 0 false, 1 true
+               obstacle_list.append(snail_surface.get_rect(bottomright= (randint(900, 1100), 300)))
+            else:
+               obstacle_list.append(fly_surface.get_rect(bottomright= (randint(900, 1100), 210)))
+                       
       else:
          if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
             game_active = True
-            snail_rectangle.left = 800
             start_time = pygame.time.get_ticks()
    
    if game_active:      
@@ -80,11 +87,6 @@ while True:
       screen.blit(sky_surface, (0, 0))
       screen.blit(ground_surface, (0, 300))
       score = display_score()
-      
-      # Snail
-      # snail_rectangle.x -= 4
-      # if snail_rectangle.right < 0: snail_rectangle.left = 800
-      # screen.blit(snail_surface, snail_rectangle)
       
       # Player
       player_gravity += 1
@@ -96,7 +98,7 @@ while True:
       obstacle_list = obstacle_movement(obstacle_list)
       
       # Collision
-      if snail_rectangle.colliderect(player_rectangle): game_active = False
+      # if snail_rectangle.colliderect(player_rectangle): game_active = False
    else:
       # Intro screen
       screen.fill((94, 129 ,162))
